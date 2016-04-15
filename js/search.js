42e872bb51ea9baca3adab621bb855858a65afe7
Vova Yatsyuk <vovayatsyuk@users.noreply.github.com>
2016-04-15 15:39:49 +0300

    Added m1 and m2 grouping in search results

--- js/search.js
+++ js/search.js
@@ -71,14 +71,35 @@ var Search = function(options) {
   };
 
   var render = function(records) {
-    var result = '';
+    var result = '',
+        m1 = '',
+        m2 = '';
+
     if (false !== records) {
       for (var i in records) {
-        result += Mustache.render(options.template, records[i]);
+        var li = Mustache.render(options.template, records[i]);
+        if (records[i].url.indexOf('/m1/') !== -1) {
+          m1 += li;
+        } else if (records[i].url.indexOf('/m2/') !== -1) {
+          m2 += li;
+        } else {
+          result += li;
+        }
       }
-      if (!result) {
+
+      if (m1) {
+        m1 = '<li class="group">Magento 1.x</li><ul>' + m1 + '</ul>';
+      }
+      if (m2) {
+        m2 = '<li class="group">Magento 2.x</li><ul>' + m2 + '</ul>';
+      }
+
+      if (!result && !m1 && !m2) {
         result = 'No results found';
+      } else {
+        result = m1 + m2 + result;
       }
+
       if (options.resultsContainer.className.indexOf('shown') === -1) {
         options.resultsContainer.className = options.resultsContainer.className + ' shown';
       }
