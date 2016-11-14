98b0c2b97ac1d65ddd8627b4995bce3abe90f803
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2016-11-14 14:37:22 +0200

    Sidebar navigation refactoring

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,22 +1,6 @@
-(function($) {
-    // Activate href link and all parent li's
-    var url = window.location.pathname,
-        link = $('[href="' + url + '"]', '.sidenav').first();
+var navigation = (function() {
 
-    if (link) {
-        var li = link.parent('li').addClass('active');
-        while ((li = li.parents('li')) && li.length) {
-            li.addClass('active');
-        }
-    }
-
-    // Activate anchor link and all parent li's
-    // Need to wait for mdl layout event to properly calculate scrollOffsets
-    $(document.body).on('mdl-componentupgraded', function(e) {
-        if (!($(e.target).hasClass('mdl-layout'))) {
-            return;
-        }
-
+    function spy(entered, state) {
         var links    = $('li a', '.sidenav'),
             sections = $('h1, h2, h3, h4, h5, h6', '.mdl-layout__content'),
             visible  = [];
@@ -33,25 +17,57 @@
             }
             visible[section.id] = section;
 
-            // get closest element to the top of the screen
+            // get closest element to the middle of the screen
 
             console.log(visible);
 
-            var link = links.filter('[href="#' + section.id + '"]');
-            if (!link.length) {
+            navigation.activate('#' + section.id);
+        }, {
+            context: $('.mdl-layout__content').get(0),
+            offset: -parseInt($('.mdl-layout__content').height() / 2),
+            contain: true
+        });
+    }
+
+    return {
+        init: function() {
+            // Activate href link and all parent li's
+            this.activate(window.location.pathname);
+
+            // Activate anchor link and all parent li's while scrolling.
+            // Need to wait for mdl layout event to properly calculate scrollOffsets.
+            $(document.body).on('mdl-componentupgraded', function(e) {
+                if (!($(e.target).hasClass('mdl-layout'))) {
+                    return;
+                }
+                spy();
+            });
+        },
+
+        /**
+         * Activate links and all parent li's
+         *
+         * @param  Element link
+         * @return void
+         */
+        activate: function(href) {
+            var container = '.sidenav',
+                link = $('[href="' + href + '"]', container).first();
+
+            if (!link) {
                 return;
             }
 
-            links.parent('li').removeClass('active');
+            $('li.active', container).removeClass('active');
 
             var li = link.parent('li').addClass('active');
             while ((li = li.parents('li')) && li.length) {
                 li.addClass('active');
             }
-        }, {
-            context: $('.mdl-layout__content').get(0),
-            offset: 0,
-            contain: true
-        });
-    });
+        }
+    }
+})();
+
+(function($) {
+    navigation.init();
 })(jQuery);
